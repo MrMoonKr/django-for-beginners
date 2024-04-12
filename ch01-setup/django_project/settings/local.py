573e74630d6d672942f396e78a0fe85b3a56fa22
Mr.Moon <MrMoonKr@gmail.com>
2024-04-12 15:15:47 +0900

    mysqlclient , environs ...

--- ch01-setup/django_project/settings/local.py
+++ ch01-setup/django_project/settings/local.py
@@ -1,4 +1,30 @@
 from .base import *
+from environs import Env
 
 print( "[정보] local settings loaded" )
 
+env = Env()
+env.read_env()
+
+DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
+    # "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": "django-for-beginners",
+    #     "USER": "root",
+    #     "PASSWORD": "password",
+    #     "HOST": "127.0.0.1",
+    #     "PORT": "3306",
+    # },
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST", "127.0.0.1"),
+        "PORT": env("DB_PORT", "3306"),
+    }
+}
